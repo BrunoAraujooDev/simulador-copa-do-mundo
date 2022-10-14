a9bcb6011522ee277821c094309c04d8f5b7620f
Bruno Araujo <bruno.aoliveira@al.infnet.edu.br>
2022-10-14 20:57:36 -0300

    finalizando o layout da pagina de playoff

--- src/view/PlayoffsStage/index.jsx
+++ src/view/PlayoffsStage/index.jsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect, useState } from "react"
 import { PlayoffContext } from "../../context/PlayoffContext"
 import { createMatches } from "../../util/playoffResults"
-import { EightRoundSection, MatchupDiv, PlayoffContainer } from "./style"
+import { RoundSection, MatchupDiv, PlayoffContainer, SpanResult } from "./style"
+import './index.css';
 
 export function PlayoffComponent() {
 
@@ -25,43 +26,7 @@ export function PlayoffComponent() {
         console.log('result', result)
 
         setTeamsQualified(result)
-
-        // setRoundEightTeams(qualifiedTeams)
-
-        // if(roundEightTeams.length > 0){
-        //     makeQuarterResults()
-        // }
     }
-
-    // async function makeQuarterResults() {
-
-    //     console.log('roundEightTeams', roundEightTeams)
-    //     const results = await makePlayoffResults(roundEightTeams, 'quarters')
-
-
-
-    //     setQuartersTeams(results)
-
-    //     if(quartersTeams.length > 0){
-    //         makeSemisResults()
-    //     }
-    // }
-
-    // async function makeSemisResults() {
-
-    //     const result = await makePlayoffResults(quartersTeams, 'semi')
-
-    //     setSemiTeams(result)
-    //     makeFinalsResult()
-
-    // }
-
-    // function makeFinalsResult() {
-
-    //     const result = makePlayoffResults(semiTeams, 'finals')
-
-    //     setSemiTeams(result)
-    // }
 
     useEffect(() => {
         dealResults()
@@ -71,187 +36,116 @@ export function PlayoffComponent() {
         <PlayoffContainer>
             {
                 <>
-                    <EightRoundSection>
+                    <RoundSection>
                         {
                             teamsQualified.round8.map((team, idx) => {
                                 if (idx < 8 && (idx % 2 == 0)) {
                                     return (
                                         <MatchupDiv key={team.name}>
-                                            <h3>{team.name}</h3>
-                                            <h4>{team.round8?.goals}</h4>
-                                            <span>X</span>
-                                            <h4>{team.round8?.goalsTaken}</h4>
-                                            <h3>{team.round8.adversary}</h3>
+                                            <h3 className={team.round8.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.round8?.goals} x {team.round8?.goalsTaken}</SpanResult>
+                                            <h3 className={team.round8.winner === true ? 'loser': 'winner' }>{team.round8.adversary}</h3>
                                         </MatchupDiv>
                                     )
                                 }
                             })
                         }
-                        </EightRoundSection>
-                        <EightRoundSection>
+                    </RoundSection>
+                    <RoundSection>
 
-                            {
-                                teamsQualified.quarters.map((team, idx) => {
-                                    if (idx < 4 && (idx % 2 == 0)) {
-                                        return (
-                                            <MatchupDiv key={team.name}>
-                                                <h3>{team.name}</h3>
-                                                <h4>{team.quarters?.goals}</h4>
-                                                <span>X</span>
-                                                <h4>{team.quarters?.goalsTaken}</h4>
-                                                <h3>{team.quarters.adversary}</h3>
-                                            </MatchupDiv>
-                                        )
-                                    }
-                                })
-                            }
-                        </EightRoundSection>
-                        <EightRoundSection>
+                        {
+                            teamsQualified.quarters.map((team, idx) => {
+                                if (idx < 4 && (idx % 2 == 0)) {
+                                    return (
+                                        <MatchupDiv key={team.name}>
+                                            <h3 className={team.quarters.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.quarters?.goals} x {team.quarters?.goalsTaken}</SpanResult>
+                                            <h3 className={team.quarters.winner === true ? 'loser': 'winner' }>{team.quarters.adversary}</h3>
+                                        </MatchupDiv>
+                                    )
+                                }
+                            })
+                        }
+                    </RoundSection>
+                    <RoundSection>
 
-                            {
-                                teamsQualified.semi.map((team, idx) => {
-                                    if (idx == 0 && (idx % 2 == 0)) {
-                                        return (
-                                            <MatchupDiv key={team.name}>
-                                                <h3>{team.name}</h3>
-                                                <h4>{team.semi?.goals}</h4>
-                                                <span>X</span>
-                                                <h4>{team.semi?.goalsTaken}</h4>
-                                                <h3>{team.semi?.adversary}</h3>
-                                            </MatchupDiv>
-                                        )
-                                    }
-                                })
-                            }
-                        </EightRoundSection>
-                        <EightRoundSection>
+                        {
+                            teamsQualified.semi.map((team, idx) => {
+                                if (idx == 0 && (idx % 2 == 0)) {
+                                    return (
+                                        <MatchupDiv key={team.name}>
+                                            <h3 className={team.semi.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.semi?.goals} x {team.semi?.goalsTaken}</SpanResult>
+                                            <h3 className={team.semi.winner === true ? 'loser': 'winner' }>{team.semi?.adversary}</h3>
+                                        </MatchupDiv>
+                                    )
+                                }
+                            })
+                        }
+                    </RoundSection>
+                    <RoundSection>
 
-                            {
-                                teamsQualified.finals.map((team, idx) => {
-                                    if (idx == 0 && (idx % 2 == 0)) {
-                                        return (
-                                            <MatchupDiv key={team.name}>
-                                                <h3>{team.name}</h3>
-                                                <h4>{team.finals?.goals}</h4>
-                                                <span>X</span>
-                                                <h4>{team.finals?.goalsTaken}</h4>
-                                                <h3>{team.finals.adversary}</h3>
-                                            </MatchupDiv>
-                                        )
-                                    }
-                                })
-                            }
-                        </EightRoundSection>
-                        <EightRoundSection>
+                        {
+                            teamsQualified.finals.map((team, idx) => {
+                                if (idx == 0 && (idx % 2 == 0)) {
+                                    return (
+                                        <MatchupDiv key={team.name}>
+                                            <h3 className={team.finals.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.finals?.goals} x {team.finals?.goalsTaken}</SpanResult>
+                                            <h3 className={team.finals.winner === true ? 'loser': 'winner' }>{team.finals.adversary}</h3>
+                                        </MatchupDiv>
+                                    )
+                                }
+                            })
+                        }
+                    </RoundSection>
+                    <RoundSection>
 
-                            {
-                                teamsQualified.semi.map((team, idx) => {
-                                    if (idx == 2 && (idx % 2 == 0)) {
-                                        return (
-                                            <MatchupDiv key={team.name}>
-                                                <h3>{team.name}</h3>
-                                                <h4>{team.semi?.goals}</h4>
-                                                <span>X</span>
-                                                <h4>{team.semi?.goalsTaken}</h4>
-                                                <h3>{team.semi.adversary}</h3>
-                                            </MatchupDiv>
-                                        )
-                                    }
-                                })
-                            }
-                        </EightRoundSection>
-                        {/*
-                        <MatchupDiv>
-                            <h3>{roundEightTeams[0].name}</h3>
-                            <h4>{roundEightTeams[0]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[1]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[1].name}</h3>
-                        </MatchupDiv>
-                         <MatchupDiv>
-                            <h3>{roundEightTeams[2].name}</h3>
-                            <h4>{roundEightTeams[2]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[3]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[3].name}</h3>
-                        </MatchupDiv>
-                        <MatchupDiv>
-                            <h3>{roundEightTeams[4].name}</h3>
-                            <h4>{roundEightTeams[4]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[5]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[5].name}</h3>
-                        </MatchupDiv>
-                        <MatchupDiv>
-                            <h3>{roundEightTeams[6].name}</h3>
-                            <h4>{roundEightTeams[6]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[7]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[7].name}</h3>
-                        </MatchupDiv> */}
-                    <EightRoundSection>
+                        {
+                            teamsQualified.semi.map((team, idx) => {
+                                if (idx == 2 && (idx % 2 == 0)) {
+                                    return (
+                                        <MatchupDiv key={team.name}>
+                                            <h3 className={team.semi.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.semi?.goals} x {team.semi?.goalsTaken}</SpanResult>
+                                            <h3 className={team.semi.winner === true ? 'loser': 'winner' }>{team.semi.adversary}</h3>
+                                        </MatchupDiv>
+                                    )
+                                }
+                            })
+                        }
+                    </RoundSection>
+                    <RoundSection>
                         {
                             teamsQualified.quarters.map((team, idx) => {
                                 if (idx > 3 && (idx % 2 == 0)) {
                                     return (
                                         <MatchupDiv key={team.name}>
-                                            <h3>{team.name}</h3>
-                                            <h4>{team.quarters?.goals}</h4>
-                                            <span>X</span>
-                                            <h4>{team.quarters?.goalsTaken}</h4>
-                                            <h3>{team.quarters?.adversary}</h3>
+                                            <h3 className={team.quarters.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.quarters?.goals} x {team.quarters?.goalsTaken}</SpanResult>
+                                            <h3 className={team.quarters.winner === true ? 'loser': 'winner' }>{team.quarters?.adversary}</h3>
                                         </MatchupDiv>
                                     )
                                 }
                             })
                         }
-                    </EightRoundSection>
-                    <EightRoundSection >
+                    </RoundSection>
+                    <RoundSection >
                         {
                             teamsQualified.round8.map((team, idx) => {
 
                                 if (idx > 7 && (idx % 2 == 0)) {
                                     return (
                                         <MatchupDiv key={team.name}>
-                                            <h3>{team.name}</h3>
-                                            <h4>{team.round8?.goals}</h4>
-                                            <span>X</span>
-                                            <h4>{team.round8?.goalsTaken}</h4>
-                                            <h3>{team.round8.adversary}</h3>
+                                            <h3 className={team.round8.winner === true ? 'winner': 'loser' }>{team.name}</h3>
+                                            <SpanResult>{team.round8?.goals} x {team.round8?.goalsTaken}</SpanResult>
+                                            <h3 className={team.round8.winner === true ? 'loser': 'winner' }>{team.round8.adversary}</h3>
                                         </MatchupDiv>
                                     )
                                 }
                             })
                         }
-                        {/* <MatchupDiv>
-                            <h3>{roundEightTeams[8].name}</h3>
-                            <h4>{roundEightTeams[8]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[9]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[9].name}</h3>
-                        </MatchupDiv>
-                        <MatchupDiv>
-                            <h3>{roundEightTeams[10].name}</h3>
-                            <h4>{roundEightTeams[10]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[11]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[11].name}</h3>
-                        </MatchupDiv>
-                        <MatchupDiv>
-                            <h3>{roundEightTeams[12].name}</h3>
-                            <h4>{roundEightTeams[12]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[13]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[13].name}</h3>
-                        </MatchupDiv>
-                        <MatchupDiv>
-                            <h3>{roundEightTeams[14].name}</h3>
-                            <h4>{roundEightTeams[14]?.round8?.goals}</h4>
-                            <span>X</span>
-                            <h4>{roundEightTeams[15]?.round8?.goals}</h4>
-                            <h3>{roundEightTeams[15].name}</h3>
-                        </MatchupDiv> */}
-                    </EightRoundSection>
+                    </RoundSection>
                 </>
             }
         </PlayoffContainer>
--- src/view/PlayoffsStage/style.js
+++ src/view/PlayoffsStage/style.js
@@ -4,19 +4,24 @@ import styled from "styled-components";
 
 export const PlayoffContainer = styled.article`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(7, 1fr);
     width: 100%;
     min-height: 100vh;
-    margin-top: 300px;
+    margin-top: 220px;
+    justify-content: space-evenly;
         
 `
 
-export const EightRoundSection = styled.section`
+export const RoundSection = styled.section`
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: space-evenly;
+    height: 800px;
+    align-items: center;
     gap: 2rem;
 `
+
+
 
 export const MatchupDiv = styled.div`
     display: flex;
@@ -24,4 +29,10 @@ export const MatchupDiv = styled.div`
     justify-content: center;
     align-items: center;
     gap: 2rem;
+`
+
+
+export const SpanResult = styled.span`
+    font-weight: bold;
+    font-size: 1.125rem;
 `
